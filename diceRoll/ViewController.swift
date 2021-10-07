b840636706ad9a8621377d519b4577a67fa49467
HenryBridges <henrybridges3@gmail.com>
2021-10-07 16:55:54 +0100

    App Completed

--- diceRoll/ViewController.swift
+++ diceRoll/ViewController.swift
@@ -9,6 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputGuess: UITextField!
+    
+    
+    @IBOutlet weak var resultText: UILabel!
+    
+    
+    @IBAction func submitGuess(_ sender: Any) {
+        inputGuess.resignFirstResponder()
+        let userGuess = Int(inputGuess.text!)
+        let diceTotal = Int.random(in: 2..<13)
+        
+        if userGuess == diceTotal {
+            resultText.text = "Correct!"
+        }else{
+            resultText.text = "Incorrect, the roll was \(diceTotal)"
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
