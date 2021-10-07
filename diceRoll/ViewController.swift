59d80f2acc4314c3629d1527138e83987b375fca
HenryBridges <sghbridg@liverpool.ac.uk>
2021-10-07 16:00:23 +0100

    Initial Commit

---
+++ diceRoll/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  diceRoll
+//
+//  Created by Harry Bridges on 07/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
